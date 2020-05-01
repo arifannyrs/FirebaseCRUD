036cc8981cfca79a5844bf0f3dee36529fb8b499
arifannyrs <arifannyrs@gmail.com>
2020-05-01 23:35:54 +0700

    firebase/config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDAChDesKEyj5L05ECgbcxoh_e_ZSzbj5Q",
+  authDomain: "fir-login-3c41a.firebaseapp.com",
+  databaseURL: "https://fir-login-3c41a.firebaseio.com",
+  projectId: "fir-login-3c41a",
+  storageBucket: "fir-login-3c41a.appspot.com",
+  messagingSenderId: "1023146737017",
+  appId: "1:1023146737017:web:aa8afdfb889a3cca43c3a8",
+  measurementId: "G-SSECT9R1C9"
+};
+
+  export default firebaseConfig;
